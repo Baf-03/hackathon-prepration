4a1ea895a1860bb7f5b5ce8360309dc1db7add13
Bilal Ahmed <123253713+Baf-03@users.noreply.github.com>
2024-06-26 05:06:29 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,32 +1,33 @@
+
+
 import express from "express";
-import cors from "cors";
 import mongoose from "mongoose";
+import cors from "cors"
+// require('dotenv').config()
 import router from "./router/index.js";
-
-import dotenv from 'dotenv';
-dotenv.config();
-
 const app = express();
-const PORT =process.env.port|| 3000;
-
-
-
-
-const URI = `mongodb+srv://bilal:bilal@cluster0.ncawirq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
-
-mongoose.connect(URI)
-mongoose.connection.on("connected",()=>console.log("DB Connected"));
-mongoose.connection.on("error",(err)=>console.log("error connecting to db")+err)
+const PORT = process.env.PORT || 5000;
 
 app.use(express.json())
-app.use(express.urlencoded({extended:true}))
+app.use(express.urlencoded({ extended: true }))
+// app.use(cors({ origin: "*" }))
+
+
+const uri = "mongodb+srv://bilal:bilal@cluster0.ncawirq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+
+mongoose.connect(uri)
+    .then(() => console.log("mongodb connected!"))
+    .catch((error) => console.log("err mongodb", error.message))
+
+// app.use(route,cors());
 app.use(cors());
-app.use(router)
+app.use(router);
 
 app.get("/",(req,res)=>{
-    res.json("server is running")
+    res.json("Running");
 })
 
-app.listen(PORT,()=>{
-    console.log("server is running at http://localhost:3000")
-})
+app.listen(PORT,() =>{
+    console.log(`Server is running on port ${PORT}`);
+    console.log(`http://localhost:${PORT}`);
+});
